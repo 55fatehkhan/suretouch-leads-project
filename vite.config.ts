04fb4deb2b16f585f482f01da9819a538067472f
55fatehkhan <55fatehkhan44@gmail.com>
2025-12-15 16:03:55 +0530

    allowHosts in vite.config file

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ["suretouch-leads-website.onrender.com"],
+  },
+  // If you're using `vite preview` on Render, you may need this too:
+  preview: {
+    allowedHosts: ["suretouch-leads-website.onrender.com"],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
