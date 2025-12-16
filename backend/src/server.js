b4f9570daafb0ac907f9badee3503c8037d476ef
Anynomous User <tiwarideepak.developer@gmail.com>
2025-12-17 01:45:33 +0530

    Vercel deployments

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,10 @@ import app from './app.js';
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// Commented fir vercel
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+// Exported fir vercel
+export default app;
